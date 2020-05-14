235203e06ef2091c50e9725b8113ddeeb5af2e68
Shayan Mohsin <shaymotionfilms@gmail.com>
2020-05-14 03:49:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-// Add your functions here
-function mapToNegativize(src) {
-  return 
+function map(array, func){
+    let mappedArray = [];
+    array.forEach(index => {
+        mappedArray.push(func(index))
+    });
+    return mappedArray;
+}
+
+function reduce(array, func, startingPoint){
+    let accumulator;
+    let index;
+
+    if(!!startingPoint){
+        accumulator = startingPoint;
+        index = 0;
+    } else {
+        accumulator = func(array[0],array[1]);
+        index = 2;
+    }
+    for(index; index< array.length; index++){
+        accumulator = func(accumulator, array[index])
+    }
+    return accumulator;
 }
